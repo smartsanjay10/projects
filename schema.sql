1f65705195a8d57db19719d0faf49d1edbb007a2
smartsanjay10 <sanjaysridhar94@gmail.com>
2026-06-06 16:21:27 +0530

    added sample data

--- schema.sql
+++ schema.sql
@@ -1,1 +1,18 @@
+create database company;
+use company;
+
+create table department (
+             department_id int primary key,
+             department_name varchar(30) NOT NULL ,
+);
+ 
+create table employees (
+             employee_id int primary key ,
+             employee_name varchar(30) NOT NULL,
+             age int ,
+             salary int NOT NULL,
+             hire_date date ,
+             department_id int,
+             FOREIGN KEY (department_id) references department(department_id)
+);
 
